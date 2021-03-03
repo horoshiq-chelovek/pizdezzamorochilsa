65a3329a549b096955db744462da88cedda96c12
idinavui <kirisha.04@mail.ru>
2021-03-03 11:30:13 +0500

    pot2

--- a seargh.py
+++ a seargh.py
@@ -1,42 +1,30 @@
 vubor = 0
 idc = 1
 eno = 0
-while idc == 1:
-    print('   ')
-    print('1 = directory')
-    print('2 = mnogo')
-    print('3 = sort')
-    vubor = input(': ')
-    if vubor == '1':
-        dire = 1
-    if vubor == "2":
-        mnogo = 1
-    if vubor == '3':
-        idc = 0
-#ypr
-mnogo = 0
-dire = 0
-#-
-if dire == 0:
-    f = open('/home/student16/Рабочий стол/a_searg')
-if dire == 1:
-    vubor = input('directoria fqla =:')
-    f = open(vubor)
+dire = 1
 cufra = 0
 text = ''
 inref = ''
-idc = 0
+while idc == 1:
+    print('   ')
+    print('1 = directory',dire)
+    print('2 = start')
+    vubor = input(': ')
+    if vubor == '1':
+        dire = 1
+    if vubor == '2':
+        idc = 0
+if dire == 1:
+    vubor = input('расположение файла =:')
+    f = open(vubor)
 for l in f:
     inref += l
 vubor = input('буква: ')
+idc = 0
 while idc < len(inref):
-    if mnogo == 0:
-        if 1 > len(vubor):
-            vubor = vubor[0]
     if inref[idc] == vubor:
         cufra += 1
         text += vubor
     idc += 1
 print(vubor,'=',cufra)
 print(text)
-vubor = input("ypokovat? = [y]/[n]")
